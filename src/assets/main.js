fe14dfc0bc052db11226f629612d3257d231e4ed
Pavel <lpsyl888l@yandex.ru>
2017-09-26 12:42:38 +0300

    attempt4

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,7 +4,7 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if (answer = '' && attempt = ''){
+    if (answer = ''; && attempt = '';){
 	setHiddenFields();
 	}
 }
@@ -19,5 +19,5 @@ function setHiddenFields() {
 	return answer;
 }
 function setMessage(message){
-	
+
 }
