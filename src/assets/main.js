3b62555d02a1a4494127241cbccbfda306034cbe
Pavel <lpsyl888l@yandex.ru>
2017-09-26 12:37:16 +0300

    attempt3

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,17 +4,20 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (answer = '' && attempt = ''){
+	setHiddenFields();
+	}
 }
 
 //implement new functions here
-if (answer = '' && attempt = ''){
-	setHiddenFields();
-}
-	function setHiddenFields() {
-		answer = Math.floor(Math.random() * (10000 - 0));
-		while (answer.length < 4){
-			answer.toString().prepend('0');
-		}
-		attempt = 0;
-		return answer;
+function setHiddenFields() {
+	answer = Math.floor(Math.random() * (10000 - 0));
+	while (answer.length < 4){
+		answer.toString().prepend('0');
 	}
+	attempt = 0;
+	return answer;
+}
+function setMessage(message){
+	
+}
