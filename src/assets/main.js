2db68aa722ae88d4c414fc35a0a88df6b1833a5d
Pavel <lpsyl888l@yandex.ru>
2017-09-26 12:23:29 +0300

    #8
    add function and condition statement

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,13 @@ function guess() {
 }
 
 //implement new functions here
+if (answer = '' && attempt = ''){
+	function setHiddenFields() {
+		answer = Math.floor(Math.random() * (10000 - 0));
+		while (answer.length < 4){
+			answer.toString().prepend('0');
+		}
+		attempt = 0;
+		return answer;
+	}
+}
